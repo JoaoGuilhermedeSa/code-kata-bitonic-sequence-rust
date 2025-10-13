21ae664f3178bee027f678ff50f8fcd37de315e1
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-10-13 12:28:35 -0300

    Adjust the k6 report to get metrics

--- test-performance-k6/scenarios/get-simple-result.js
+++ test-performance-k6/scenarios/get-simple-result.js
@@ -9,7 +9,7 @@ export let GetCustomerSuccessRate = new Rate('get_customer_success_rate');
 export let GetCustomerReqs = new Rate('get_customer_reqs');
 
 export default function GetSimpleResult() {
-    const url = 'http://localhost:8000/bitonic?numbers=1,3,8,12,4,2';
+    const url = 'http://localhost:3000/bitonic?n=5&l=3&r=10';
     const params = {
         headers: {
             'Content-Type': 'application/json',
@@ -19,3 +19,14 @@ export default function GetSimpleResult() {
     let res = http.get(url, params);
     GetCustomerDuration.add(res.timings.duration);
     GetCustomerReqs.add(1);
+    GetCustomerFailRate.add(res.status !== 200);
+    GetCustomerSuccessRate.add(res.status === 200);
+
+    if(!check(res, {
+        'is status 200': (r) => r.status === 200,
+    })){
+        fail('GetSimpleResult failed');
+    };
+
+    sleep(1);
+} 
