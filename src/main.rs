984ac65c6114df36162e5998ee1318886e2b8d18
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-10-18 16:59:29 -0300

    Cleanup unused code

--- src/main.rs
+++ src/main.rs
@@ -51,27 +51,14 @@ async fn bitonic_handler(
     State(mut conn): State<redis::aio::MultiplexedConnection>,
     Query(params): Query<Params>
 ) -> Result<Json<BitonicResponse>, StatusCode> {
-    //TODO Maybe use proxy, and create a singleton connection?
-
-    //TODO Handle connection errors
-    //Check Redis connection
-    /*let mut conn = redis::Client::open("redis://redis:6379/")
-        .unwrap()
-        .get_multiplexed_async_connection()
-        .await
-        .unwrap();*/
 
     //Create a unique key for the parameters
     let key = format!("bitonic:{}:{}:{}", params.n, params.l, params.r);
 
     //Check if the result is already cached
-    //let result: redis::RedisResult<Option<String>> = conn.get(&key).await;
-
-    //if let Ok(Some(cached)) = result {
     if let Ok(Some(cached)) = conn.get::<_, Option<String>>(&key).await {
         println!("Result already cached on Redis. Key: {:?}", key);
-        //let result: Vec<i32> = serde_json::from_str(&cached).unwrap();
-        //return Json(BitonicResponse { input: params, result });
+
         if let Ok(result) = serde_json::from_str(&cached) {
             return Ok(
                 Json(BitonicResponse {
@@ -84,11 +71,8 @@ async fn bitonic_handler(
 
     //Compute the result if not cached
     let result = get_bitonic_sequence_v2(params.n, params.l, params.r);
+
     //Store the result in Redis
-    /*let _: () = conn
-        .set_ex(&key, serde_json::to_string(&result).unwrap(), 3600)// Set an expiration time of 1 hour
-        .await
-        .unwrap();*/
     if let Ok(result_json) = serde_json::to_string(&result) {
         if let Err(e) = conn.set_ex::<_, _, ()>(&key, result_json, 3600).await {
             eprintln!("Failed to cache result in Redis: {}", e);
@@ -104,35 +88,20 @@ async fn bitonic_handler(
 }
 
 //list all cached results
-//async fn cache_list_handler() -> impl IntoResponse {
 async fn cache_list_handler(State(mut conn): State<redis::aio::MultiplexedConnection>) -> Result<
     Json<Vec<(String, Option<String>)>>,
     StatusCode
 > {
-    /*let mut conn = redis::Client::open("redis://redis:6379/")
-        .unwrap()
-        .get_multiplexed_async_connection()
-        .await
-        .unwrap();*/
 
     let keys: Vec<String> = conn.keys("bitonic:*").await.unwrap_or_default();
-
     let mut results = Vec::new();
 
-    /*for key in keys {
-        let result: redis::RedisResult<Option<String>> = conn.get(&key).await;
-
-        if let Ok(value) = result {
-            results.push((key, value));
-        }
-    }*/
     for key in keys {
         if let Ok(value) = conn.get::<_, Option<String>>(&key).await {
             results.push((key, value));
         }
     }
 
-    //Json(results)
     Ok(Json(results))
 }
 
